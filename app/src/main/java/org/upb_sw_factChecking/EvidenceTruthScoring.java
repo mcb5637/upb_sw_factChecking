3da5664e5e015e45a87773bd609bf01900839f62
ksczsw <kaja.czolkos@web.de>
2025-01-11 15:54:28 +0100

    correct that 0.0 is strongest and 1.0 weakest weight

--- app/src/main/java/org/upb_sw_factChecking/EvidenceTruthScoring.java
+++ app/src/main/java/org/upb_sw_factChecking/EvidenceTruthScoring.java
@@ -5,8 +5,8 @@ import org.apache.jena.rdf.model.Statement;
 
 public class EvidenceTruthScoring {
     // finds the strongest positive and negative rule that covers the statement and computes the truthScore
-    double maxPositiveW;
-    double maxNegativeW;
+    double minPositiveW;
+    double minNegativeW;
     Statement statement;
     //Rule[] rules; TODO add Rulelist/array etc
 
@@ -19,36 +19,38 @@ public class EvidenceTruthScoring {
     }
 
     public void findStrongestCoveringRules(){
-        double ruleWeight = 0.0;
-        double currentMaxPosW = 0.0;
-        double currentMaxNegW = 0.0;
+        double ruleWeight = 1.0;
+        double currentMinPosW = 1.0;
+        double currentMinNegW = 1.0;
         // TODO iterate through all rules. If rule covers Statement, compute weight "ruleWeight"/look up weight
         // TODO check if rule is pos rule or neg rule
         // depends on how rules are implemented
 
+        //the lower the weight, the stronger the rule
+
         // if ruletype pos:
-        if (ruleWeight > currentMaxPosW){
-            currentMaxPosW = ruleWeight;
+        if (ruleWeight < currentMinPosW){
+            currentMinPosW = ruleWeight;
         }
         // if ruletype neg:
-        if (ruleWeight > currentMaxPosW){
-            currentMaxNegW = ruleWeight;
+        if (ruleWeight < currentMinPosW){
+            currentMinNegW = ruleWeight;
         }
         // change class variables at the end
-        maxPositiveW = currentMaxPosW;
-        maxNegativeW = currentMaxNegW;
+        minPositiveW = currentMinPosW;
+        minNegativeW = currentMinNegW;
     }
 
 
-    public double computeScore(double maxPositiveW, double maxNegativeW) {
+    public double computeScore(double minPositiveW, double minNegativeW) {
         final double truthScore;
-        if (maxPositiveW != 0){
+        if (minPositiveW != 1.0){
             // "If positive evidence for a given statement are found in a knowledge graph, then we use
             //0.0 instead of the weight of the strongest negative rule"
-            truthScore = (1-maxPositiveW-2)/2;
+            truthScore = (1-minPositiveW-2)/2;
         }
         else{
-            truthScore = ((1-maxPositiveW)-(1-maxNegativeW)+1)/2;
+            truthScore = ((1-minPositiveW)-(1-minNegativeW)+1)/2;
         }
         return truthScore;
 
