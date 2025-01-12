f9434df5ce6cf3a5c1f38074a4c110680050780c
ksczsw <kaja.czolkos@web.de>
2025-01-12 14:26:43 +0100

    add full findStrongestCoveringRules method, update EvidenceTruthScoring class

--- app/src/main/java/org/upb_sw_factChecking/EvidenceTruthScoring.java
+++ app/src/main/java/org/upb_sw_factChecking/EvidenceTruthScoring.java
@@ -1,49 +1,57 @@
 package org.upb_sw_factChecking;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Statement;
+import org.upb_sw_factChecking.scoring.WeightedRule;
 
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class EvidenceTruthScoring {
-    // finds the strongest positive and negative rule that covers the statement and computes the truthScore
-    double minPositiveW;
-    double minNegativeW;
-    Statement statement;
-    //Rule[] rules; TODO add Rulelist/array etc
+    // finds the strongest positive and negative rule from a rule file that covers the statement and computes the truthScore
+    Path ruleFile;
+    WeightedRule[] rules;
 
-
-
-    public EvidenceTruthScoring(Statement statement, String[] rules){ //TODO change rules type
-        this.statement = statement;
-        //this.rules = rules;
-
+    public EvidenceTruthScoring(Path ruleFile){
+        Model m = ModelFactory.createDefaultModel();
+        try {
+            this.ruleFile = ruleFile;
+            rules = WeightedRule.loadRules(ruleFile, m);
+        } catch (IOException e) {
+            System.out.println("Problem loading the rules" + e.getMessage());
+        }
     }
 
-    public void findStrongestCoveringRules(){
-        double ruleWeight = 1.0;
+
+    public double[] findStrongestCoveringRules(Statement statement){
+        //the lower the weight, the stronger the rule: min 0.0, max 1.0
         double currentMinPosW = 1.0;
         double currentMinNegW = 1.0;
-        // TODO iterate through all rules. If rule covers Statement, compute weight "ruleWeight"/look up weight
-        // TODO check if rule is pos rule or neg rule
-        // depends on how rules are implemented
-
-        //the lower the weight, the stronger the rule
-
-        // if ruletype pos:
-        if (ruleWeight < currentMinPosW){
-            currentMinPosW = ruleWeight;
+        //iterate through all rules
+        for (WeightedRule r: rules){
+            //check if rule covers statement
+            boolean covers = r.doesRuleApply(statement);
+            if(covers){
+                //update weight minimum
+                if (r.weight < currentMinPosW && r.isPositive){
+                    currentMinPosW = r.weight;
+                }
+                else if (r.weight < currentMinNegW && !r.isPositive) {
+                    currentMinNegW = r.weight;
+                }
+            }
         }
-        // if ruletype neg:
-        if (ruleWeight < currentMinPosW){
-            currentMinNegW = ruleWeight;
-        }
-        // change class variables at the end
-        minPositiveW = currentMinPosW;
-        minNegativeW = currentMinNegW;
+        //return the weight of the strongest positive and negative rule, that covers the statement
+        return new double[]{currentMinPosW,currentMinNegW};
     }
 
 
-    public double computeScore(double minPositiveW, double minNegativeW) {
+    public static double computeScore(double[] strongest) {
         final double truthScore;
+        double minPositiveW = strongest[0];
+        double minNegativeW = strongest[1];
+
         if (minPositiveW != 1.0){
             // "If positive evidence for a given statement are found in a knowledge graph, then we use
             //0.0 instead of the weight of the strongest negative rule"
