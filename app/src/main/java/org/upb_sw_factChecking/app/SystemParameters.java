da76b17cd4f91d69af3b2341f91e5381fae00d7e
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2025-01-13 02:34:31 +0100

    Increase default number of threads

--- app/src/main/java/org/upb_sw_factChecking/app/SystemParameters.java
+++ app/src/main/java/org/upb_sw_factChecking/app/SystemParameters.java
@@ -9,9 +9,9 @@ public class SystemParameters {
     // Local graph generation will start with this path length. If no path has been found,
     // the path length will be increased by one until the absolute maximum path length is reached.
     public static final int INITIAL_MAX_PATH_LENGTH = 3;
-    public static final int ABSOLUTE_MAX_PATH_LENGTH = 10;
+    public static final int ABSOLUTE_MAX_PATH_LENGTH = 5;
 
     // Number of threads to use for rule generation.
     // Something will create additional threads anyway, so shouldn't be set too high.
-    public static final int RULE_GEN_THREAD_COUNT = Runtime.getRuntime().availableProcessors() / 4;
+    public static final int RULE_GEN_THREAD_COUNT = Runtime.getRuntime().availableProcessors() / 2;
 }
